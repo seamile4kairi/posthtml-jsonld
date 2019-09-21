ef98b737f54d1e01418e330b45c25d9484d3b4d5
Kairi KAWASAKI <seamile.rivercape@gmail.com>
2019-09-21 13:48:47 +0900

    [change] Simplified the testing function

--- test/index.js
+++ test/index.js
@@ -1,27 +1,34 @@
 'use strict'
 
-const test = require('ava')
+const tester = require('ava')
 const Builder = require('./util/Builder')
 
-test('index.html', async t => {
-  const builder = new Builder('index')
-  const result = await builder.run({
-    root: 'test/fixtures',
-    host: 'https://github.com',
-    base: '/seamile4kairi/posthtml-jsonld',
-    title: {},
-    description: {},
-    opengraph: {},
-    twittercards: {},
-    canonical: true,
-    alternate: [{
-      media: 'only screen and (max-width: 560px)',
-      href: url => url.replace(/\/$/, '') + '/sp/'
-    }, {
-      hreflang: 'en',
-      href: url => url.replace(/\/$/, '') + '/en/'
-    }]
-  })
+const _config = {
+  root: 'test/fixtures',
+  host: 'https://github.com',
+  base: '/seamile4kairi/posthtml-jsonld'
+}
 
-  return t.is(builder.expect, result.html)
-})
+test('index', Object.assign(_config, {
+  title: {},
+  description: {},
+  opengraph: {},
+  twittercards: {},
+  canonical: true,
+  alternate: [{
+    media: 'only screen and (max-width: 560px)',
+    href: url => url.replace(/\/$/, '') + '/sp/'
+  }, {
+    hreflang: 'en',
+    href: url => url.replace(/\/$/, '') + '/en/'
+  }]
+}))
+
+function test (name, config) {
+  return tester(name, async t => {
+    const builder = new Builder(name)
+    const result = await builder.run(config)
+
+    return t.is(builder.expect, result.html)
+  })
+}
